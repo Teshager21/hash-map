7358fff4bc10823f105760fd89781b53f832823e
teshager <teshager8922@gmail.com>
2024-08-10 13:54:50 +0300

    add a get method to retreive values using keys

--- HashMap.js
+++ HashMap.js
@@ -1,10 +1,12 @@
 class HashMap{
     constuctor(){
+    this.size=0
 
     }
 
 #bucket_size=16;
 #bucket=Array(this.#bucket_size).fill(null);
+size=0;
 
 
 #hash(key){ 
@@ -22,17 +24,49 @@ set(key,value){
     const index=this.#hash(key);
     let entry= this.#bucket[index];
     if(!entry){
-        this.#bucket[index]={key,value};
+        this.#bucket[index]={[key]:value};
+        this.size++;
     }
     else if(Array.isArray(entry)){
-        this.#bucket.push({key,value});
+        let found=true;
+        for(let pair of entry){
+            found= true;
+            if(Object.keys(pair).includes(key)) {
+                this.#bucket[index][entry.indexOf(pair)]={[key]:value};
+                break;
+             }
+          found=false;
+        }
+        if (!found){
+            this.#bucket[index].push({[key]:value});
+            this.size++;
+        }
     }else{
         if(Object.keys(entry).includes(key)) entry={key,value};
-        else this.#bucket[index]=[entry,{key,value}]
+        else this.#bucket[index]=[entry,{[key]:value}]
     }
    
-    console.log(`hash for ${key} is ${index}`);
-    console.log(this.#bucket)
+    // console.log(`hash for ${key} is ${index}`);
+    // console.log(this.#bucket)
+}
+
+get(key){
+    const index= this.#hash(key);
+    const bucket=this.#bucket[index];
+    if(!bucket){
+        console.error(`No entry with key '${key}' found`);
+       return;
+    }
+    if(!Array.isArray(bucket)){
+        return bucket[key];
+    }else{
+    for(let entry of bucket){
+        if(Object.keys(entry).includes(key)) {
+            return entry[key];
+        }
+    }
+        
+    }
 }
 
 }
@@ -42,4 +76,8 @@ hash.set('Mosh',25);
 hash.set('Bill',24);
 hash.set('Fani',56);
 hash.set('Sara',55);
-console.log(hash);
+console.log('======================================');
+hash.set('Mosh',100)
+// console.log(hash);
+console.log(hash.size)
+console.log('getting',hash.get('Fani'));
